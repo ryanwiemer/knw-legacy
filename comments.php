2aefcb1f4a05192f56ccfd5856410e4009dbb25f
Ryan Wiemer <ryan@ryanwiemer.com>
2014-05-05 16:42:09 -0700

    Updating the comment system. Styles still need updates

---
+++ comments.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div class="comments">
+
+	<?php // You can start editing here -- including this comment! ?>
+
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'knw' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h2>
+		<ol class="comments__list">
+			<?php
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				) );
+			?>
+		</ol><!-- .comment__list -->
+	<?php endif; // have_comments() ?>
+
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="comments__closed"><?php _e( 'Comments are closed.', 'knw' ); ?></p>
+	<?php endif; ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- comments -->
--- single.php
+++ single.php
@@ -18,6 +18,21 @@ get_header(); ?>
       get_template_part( 'content', 'blog' );
       }?>
 
+      <?php
+        $comments_args = array (
+          'id_form' => '',
+          'id_submit' => '',
+          'title_reply' => 'Comment',
+          'label_submit' => __( 'Submit' ),
+           'comment_notes_after' => '',
+
+        );
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comment_form($comments_args);
+				endif;
+			?>
+
 		<?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
