5f2e615e52eee7fe02f7cb634e2670c0f8d37be1
Ryan Wiemer <ryan@ryanwiemer.com>
2014-06-07 18:58:09 -0700

    fixing css class on footer

--- header.php
+++ header.php
@@ -18,10 +18,10 @@
  	 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
 
     <!--[if lt IE 9]>
-			<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/oldie.min.css" />
-			<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.min.js" type="text/javascript"></script>
+      <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/oldie.min.css" />
+      <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.min.js" type="text/javascript"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/js/selectivizr.min.js" type="text/javascript"></script>
-		<![endif]-->
+    <![endif]-->
 
 		<!-- Google Analytics -->
     <script>
