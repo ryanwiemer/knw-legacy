f90692504cc9ba4c04a10609e15de2095b0e03e3
Ryan Wiemer <ryan@ryanwiemer.com>
2014-08-03 17:17:35 -0700

    fixed typo

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
     require_once('../../../wp-load.php');
     $to = get_option( 'admin_email' );
     $name =  sanitize_text_field( $_POST['name'] );
-    $subject = "KNW Photogrpahy Inquiry From $name";
+    $subject = "KNW Photography Inquiry From $name";
     $headers = "From: " . sanitize_email( $_POST["email"] ) . "\r\n";
     $headers .= "Reply-To: ". sanitize_email( $_POST["email"] ) . "\r\n";
     $body = esc_textarea( $_POST["message"] );
