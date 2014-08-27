b0c7df6a098ce51181edf2acb5354923c82a1198
Ryan Wiemer <ryan@ryanwiemer.com>
2014-08-26 22:35:10 -0700

    Fixing wp_query on Blog and Galleries pages

--- pages/Blog.php
+++ pages/Blog.php
@@ -9,76 +9,63 @@ get_header(); ?>
 </div>
 
 <div class="content">
-
-<?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1; // allow for pagination
-
-$args = array(
-'post_type' => 'post',
-'paged' => $paged,
-
-'tax_query' => array(
-array(
-'taxonomy' => 'post_format',
-'field' => 'slug',
-'terms' =>  'post-format-gallery',
-'operator' => 'NOT IN'
-)
-),
-'posts_per_page' => 3,
-'no_found_rows' => true, // counts posts, remove if pagination required
-'update_post_term_cache' => false, // grabs terms, remove if terms required (category, tag...)
-'update_post_meta_cache' => false, // grabs post meta, remove if post meta required
-);
-?>
-
-<?php
-  $temp_query = $wp_query;
-  $wp_query= null;
-?>
-<?php $wp_query = new WP_Query( $args ); ?>
-
 <section class="blog-list">
-  <?php if ( have_posts() ) : ?>
-  <!-- the loop -->
-  <?php while ( have_posts() ) : the_post(); ?>
-  <article class="blog-post">
-    <?php if (has_post_thumbnail( $post->ID ) ): ?>
-    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-    $image = $image[0]; ?>
-    <?php else :
-    $image = get_bloginfo( 'stylesheet_directory') . '/assets/img/placeholder.png'; ?>
-    <?php endif; ?>
-    <?php if ( has_post_thumbnail() ) {
-        the_post_thumbnail( 'thumbnail', array( 'class' => 'blog-post__img' ) ); }
-        else {
-          echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/img/placeholder.png"  class="blog-post__img"/>';
-          }?>
-    <div class="blog-post__details">
-      <h2 class="blog-post__title"><?php the_title(); ?></h2>
-      <p class="blog-post__date"><?php echo get_the_date(); ?></p>
-      <p class="blog-post__excerpt"><?php echo comments_number(); ?></p>
-      <a class="blog-post__btn btn btn--small" href="<?php the_permalink(); ?>">
-        Read more...
-      </a>
-    </div>
-  </article>
-  <?php endwhile; ?>
-  <!-- end of the loop -->
-</section>
-<div class="pagination">
+
 <?php
-  next_posts_link( 'Older Entries', 9999);
-  previous_posts_link( 'Newer Entries' );
+  $args = array(
+  'post_type' => 'post',
+  'paged' => $paged,
+  'tax_query' => array(
+  array(
+  'taxonomy' => 'post_format',
+  'field' => 'slug',
+  'terms' =>  'post-format-gallery',
+  'operator' => 'NOT IN'
+  )
+  ),
+  'posts_per_page' => 3,
+  );
+    $temp = $wp_query;
+    $wp_query = null;
+    $wp_query = new WP_Query();
+    $wp_query->query($args);
 ?>
+
+    <?php if ( have_posts() ) : ?>
+    <!-- the loop -->
+    <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+    <article class="blog-post">
+
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); $image = $image[0]; ?>
+      <?php else :
+        $image = get_bloginfo( 'stylesheet_directory') . '/assets/img/placeholder.png'; ?>
+      <?php endif; ?>
+
+      <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'thumbnail', array( 'class' => 'blog-post__img' ) ); }
+      else { echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/img/placeholder.png"  class="blog-post__img"/>';}?>
+
+      <div class="blog-post__details">
+        <h2 class="blog-post__title"><?php the_title(); ?></h2>
+        <p class="blog-post__date"><?php echo get_the_date(); ?></p>
+        <p class="blog-post__excerpt"><?php echo comments_number(); ?></p>
+        <a class="blog-post__btn btn btn--small" href="<?php the_permalink(); ?>">Read more...</a>
+      </div>
+    </article>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+</section>
+
+<div class="pagination">
+  <?php
+    next_posts_link( 'Older Entries');
+    previous_posts_link( 'Newer Entries' );
+  ?>
 </div>
-<?php else:  ?>
+
+<?php $wp_query = null; $wp_query = $temp;  // Reset ?>
+<?php else: ?>
   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
 
-<?php
-  $wp_query = null;
-  $wp_query = $temp_query;
-?>
-<?php wp_reset_postdata(); ?>
 <?php get_footer(); ?>
--- pages/Galleries.php
+++ pages/Galleries.php
@@ -12,8 +12,6 @@ get_header(); ?>
 <div class="content">
 
 <?php
-	// First, initialize how many posts to render per page
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; // allow for pagination
   $args = array(
   'post_type' => 'post',
   'paged' => $paged,
@@ -22,28 +20,22 @@ get_header(); ?>
   'taxonomy' => 'post_format',
   'field' => 'slug',
   'terms' =>  'post-format-gallery',
-  'operator' => 'IN'
   )
   ),
   'posts_per_page' => 6,
-  'no_found_rows' => true, // counts posts, remove if pagination required
-  'update_post_term_cache' => false, // grabs terms, remove if terms required (category, tag...)
-  'update_post_meta_cache' => false, // grabs post meta, remove if post meta required
   );
+    $temp = $wp_query;
+    $wp_query = null;
+    $wp_query = new WP_Query();
+    $wp_query->query($args);
 ?>
-
-<?php
-  $temp_query = $wp_query;
-  $wp_query= null;
-?>
-<?php $wp_query = new WP_Query( $args ); ?>
 
 <?php if ( have_posts() ) : ?>
 
 <?php $cat_args = array(
-	'orderby'            => 'count',
-	'title_li'           => __( '' ),
-	'show_option_none'   => __( 'No categories' ),
+  'orderby'            => 'count',
+  'title_li'           => __( '' ),
+  'show_option_none'   => __( 'No categories' ),
   'include'            => '',
   'exclude'            => '9'
 ); ?>
@@ -56,37 +48,34 @@ get_header(); ?>
   </ul>
 </div>
 <section class="gallery-list">
-  <?php while ( have_posts() ) : the_post(); ?>
-		<article class="gallery">
-			<a href="<?php the_permalink(); ?>">
-				<div class="gallery__border">
-					<?php if ( has_post_thumbnail() ) {
+  <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+    <article class="gallery">
+      <a href="<?php the_permalink(); ?>">
+        <div class="gallery__border">
+          <?php if ( has_post_thumbnail() ) {
               the_post_thumbnail( 'thumbnail', array( 'class' => 'gallery__image' ) ); }
               else {
                 echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/img/placeholder.png"  class="gallery__image"/>';
                 }?>
-				  <div class="gallery__overlay">
-						<h3 class="gallery__title"><?php the_title(); ?></h3>
-					</div>
-				</div>
-			</a>
-		</article>
+          <div class="gallery__overlay">
+            <h3 class="gallery__title"><?php the_title(); ?></h3>
+          </div>
+        </div>
+      </a>
+    </article>
   <?php endwhile; ?>
 </section>
     <!-- end of the loop -->
 <div class="pagination">
-<?php
-  next_posts_link( 'Older Entries', 99999 );
-  previous_posts_link( 'Newer Entries' );
-?>
+  <?php
+    next_posts_link( 'Older Entries' );
+    previous_posts_link( 'Newer Entries' );
+  ?>
 </div>
+
+<?php $wp_query = null; $wp_query = $temp;  // Reset ?>
 <?php else:  ?>
-  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <p><?php _e( 'Sorry, no galleries matched your criteria.' ); ?></p>
 <?php endif; ?>
 
-<?php
-  $wp_query = null;
-  $wp_query = $temp_query;
-?>
-<?php wp_reset_postdata(); ?>
 <?php get_footer(); ?>
