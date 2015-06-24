d6659fa0a75e390ee6c34eb8373d71450ef8e76b
Ryan Wiemer <ryan@ryanwiemer.com>
2015-06-24 01:35:13 -0700

    More working on cleaning up styling for the new design tweaks

--- pages/About.php
+++ pages/About.php
@@ -13,42 +13,29 @@ get_header(); ?>
 
 <section class="about-container">
 
+  <div class="about-collage about-collage--portrait">
+       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/kirsten-noelle-wiemer-knw-photography.jpg" alt="Kirsten Noelle Wiemer knw photography San Fancisco East Bay photographer"/>
+  </div>
+
 			<div class="about-collage about-collage--bio">
-			<div class="about-collage__ornament"></div>
-		  		<div class="about-collage__content">
 		  			<p><span>M</span>y name is Kirsten Noelle Wiemer and I started KNW
-		  			Photography to share my love for capturing moments with others. I have
-		  			always been a creative person and photography is one way I like to
-		  			show my creativity. Recently I discovered a love for natural
-		  			light photography and working with people. Through my photography
-		  			I hope to capture real connections and emotions of everyone I work for.
+		  			Photography to share my love of capturing moments with others. I have
+		  			always been a creative person and photography allows me to create
+            images that show real connections and emotions. I particularly
+            enjoy using natural light and working with clients
+            that share my passion for quality work.
 		  			</p>
 
   					<p>Outside of photography some things I love include making pizza,
-  					meeting new people and working on the potter’s wheel. I’m a recent
-  					college grad and also a recent transplant to the Bay Area along with
-  					my husband Ryan. We are currently living just outside of San Francisco
-  					in the East Bay and I absolutely love exploring the area. Nothing
-  					beats Northern California weather!</p>
+  					meeting new people and working on the potter’s wheel. I currently live
+            in the San Franisco Bay Area along with my husband Ryan. Online I
+            like to share my life through my personal blog where I take
+            candid photos and talk about whatever is on my mind. To read more about
+            that you can visit <a href="http://www.kandrblog.com">K&amp;R Blog</a>
+            or click the link in the menu. I look forward to hearing from you
+            regarding new photo opportunities or even if it’s just to say “hello”.</p>
 
-  					<p>Online I like to share my life through my personal blog where
-  					I share my adventures and other ramblings of my mind. If you’d like to
-  					find out more about me you can check out my personal writings on
-  					<a href="http://www.kandrblog.com">K&amp;R Blog</a>. I look forward to
-  					hearing from you regarding new photo opportunities or even if it’s
-  					just to say “hi.”
-  					</p>
-		  		</div>
+            <h3 class="signature">Kirsten Noelle Wiemer</h3>
 			</div>
-
-			<div class="about-collage about-collage--portrait">
-		  		<div class="about-collage__content">
-		  			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/kirsten-noelle-wiemer-knw-photography.jpg" alt="Kirsten Noelle Wiemer knw photography San Fancisco East Bay photographer"/>
-		  			<div class="about-collage--portrait__container">
-		  				<h3>Kirsten Wiemer</h3>
-		  			</div>
-		  		</div>
-			</div>
-
 		</section>
 <?php get_footer(); ?>
--- pages/Home.php
+++ pages/Home.php
@@ -29,6 +29,6 @@ get_header(); ?>
 </div> <!-- slider -->
 
 <?php while ( have_posts() ) : the_post(); ?>
-  <?php the_content(); ?>
+
 <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
