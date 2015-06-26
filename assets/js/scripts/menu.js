9d06b7247763ba5f4e477f43569760861f783362
Ryan Wiemer <ryan@ryanwiemer.com>
2015-06-26 01:12:32 -0700

    small menu only

--- assets/js/scripts/menu.js
+++ assets/js/scripts/menu.js
@@ -4,16 +4,3 @@ $(document).ready(function() {
     $('body').toggleClass('active');
   });
 });
-
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 75) {
-        //clearHeader, not clearheader - caps H
-        $(".menu").addClass("menu--small");
-    }
-    if (scroll < 75) {
-        //clearHeader, not clearheader - caps H
-        $(".menu").removeClass("menu--small");
-    }
-});
