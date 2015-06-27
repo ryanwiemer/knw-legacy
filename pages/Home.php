42f5b6068038d8c1fc585ffd1f75a0f5005479fc
Ryan Wiemer <ryan@ryanwiemer.com>
2015-06-27 14:53:24 -0700

    testing out logos

--- pages/Home.php
+++ pages/Home.php
@@ -4,16 +4,21 @@
  * Description: A Page Template for the Home Page
  */
 get_header(); ?>
-<img class="logo--full" src="<?php echo get_template_directory_uri(); ?>/assets/img/knw.png" />
+<img class="logo--full" src="<?php echo get_template_directory_uri(); ?>/assets/img/test-logo.svg" />
 <div class="slider">
   <?php if(get_field('slider')): ?>
   <?php while(has_sub_field('slider')): ?>
   <?php $large = wp_get_attachment_image_src(get_sub_field('slide_image'), 'large'); ?>
-    <div class="slide" style="background-image: url('<?php echo $large[0]; ?>');"></div>
+    <div class="slide" style="background-image: url('<?php echo $large[0]; ?>');">
+    <div class="slide--overlay"></div>
+    </div>
   <?php endwhile; ?>
   <?php endif; ?>
+
 </div>
-  <a class="scroll-down" href="#content">down arrow</a>
+<div class="scroll-down ">
+  <a class="icon-" href="#content">&#xe600;</a>
+</div>
 
 <div id="content" class="content">
 <?php while ( have_posts() ) : the_post(); ?>
