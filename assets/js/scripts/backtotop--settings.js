a8b8f60b51b6b759b70287327d38d0773e9ae4af
Ryan Wiemer <ryan@ryanwiemer.com>
2015-07-17 18:54:58 -0700

    Edits to the return to top button

--- assets/js/scripts/backtotop--settings.js
+++ assets/js/scripts/backtotop--settings.js
@@ -1,18 +1,11 @@
 jQuery(document).ready(function($){
-  	// browser window scroll (in pixels) after which the "back to top" link is shown
   	var offset = 300,
-  		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
   		offset_opacity = 1200,
-  		//duration of the top scrolling animation (in ms)
   		scroll_top_duration = 700,
-  		//grab the "back to top" link
   		$back_to_top = $('.cd-top');
-
-  	//hide or show the "back to top" link
   	$(window).scroll(function(){
   		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible');
   	});
-
   	//smooth scroll to top
   	$back_to_top.on('click', function(event){
   		event.preventDefault();
@@ -21,5 +14,4 @@ jQuery(document).ready(function($){
   		 	}, scroll_top_duration
   		);
   	});
-
   });
