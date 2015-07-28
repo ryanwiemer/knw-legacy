cb64030171c73dc6bcb1a4afb11b840a9327e251
Ryan Wiemer <ryan@ryanwiemer.com>
2015-07-28 15:29:13 -0700

    Fix typos on pricing page

--- pages/Pricing.php
+++ pages/Pricing.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	<div class="pricing-table">
 	  <h3 class="pricing-table__header">Portrait Packages</h3>
 		<ul class="pricing-table__list">
-			<li class="pricing-table__top-row"><h4 class="pricing-table__list-item--description">Session Description</h4><h4 class="pricing-table__list-item--price">Price</h4></li>
+			<li class="pricing-table__top-row"><h4 class="pricing-table__list-item--description">Description</h4><h4 class="pricing-table__list-item--price">Price</h4></li>
 			<li class="pricing-table__odd"><div class="pricing-table__list-item"><span>Individual Portrait</span>For high school senior photos, maternity, headshots, and others. This session is for an individual for 60 minutes.</div><div class="pricing-table__list-item--price">$250</div></li>
 			<li class="pricing-table__even"><div class="pricing-table__list-item"><span>Couples / Engagements </span>This session includes 1 location for 60 - 90 minutes. I often customize packages for couples so if you need to extend the time or travel to various locations I would love to create a customized package for you.</div><div class="pricing-table__list-item--price">$300</div></li>
 			<li class="pricing-table__odd"><div class="pricing-table__list-item"><span>Family Portrait</span>Family sessions include up to 4 people (each additional person $25) for 60 minutes. I love working outdoors but I am also willing to shoot inside your home to accommodate your needs.</div><div class="pricing-table__list-item--price">$325</div></li>
@@ -23,7 +23,7 @@ get_header(); ?>
     <ul>
       <li>A $100 non-refundable deposit is required to secure your session date.</li>
       <li>Each portrait package comes with a USB of edited high resolution color and black and white photos from your session. Printing rights included.</li>
-      <li>Travel fees may apply outside of the San Francisco East Bay Area. Additional time and locations are available upon request on any session.</li>
+      <li>Travel fees may apply outside of the San Francisco East Bay Area. Additional time and locations are available upon request on any package.</li>
     </ul>
   </div>
 </section>
@@ -32,7 +32,7 @@ get_header(); ?>
 	<div class="pricing-table">
 	<h3 class="pricing-table__header">Wedding Packages</h3>
 		<ul class="pricing-table__list">
-			<li class="pricing-table__top-row"><h4 class="pricing-table__list-item--description">Session Description</h4><h4 class="pricing-table__list-item--price">Price</h4></li>
+			<li class="pricing-table__top-row"><h4 class="pricing-table__list-item--description">Description</h4><h4 class="pricing-table__list-item--price">Price</h4></li>
 			<li class="pricing-table__odd"><div class="pricing-table__list-item"><span>Bridals / Formals</span>This session includes 1 location for a bride / groom or a bride and groom in formal attire for 60 minutes. The session can be done prior to the wedding or on the actual wedding day.</div><div class="pricing-table__list-item--price">$350</div></li>
 			<li class="pricing-table__even"><div class="pricing-table__list-item"><span>Basic Wedding Package</span>This package includes 4 hours of wedding day coverage.</div><div class="pricing-table__list-item--price">$1500</div></li>
 			<li class="pricing-table__odd"><div class="pricing-table__list-item"><span>Complete Wedding Package</span>This package includes 6 hours or more of wedding day coverage.</div><div class="pricing-table__list-item--price">$2100+</div></li>
