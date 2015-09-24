95db75b4ad8a645fd0f6c890797adf787f47dff7
Ryan Wiemer <ryan@ryanwiemer.com>
2015-09-23 21:22:49 -0700

    Updating social media links

--- footer.php
+++ footer.php
@@ -7,9 +7,9 @@
 <footer class="site-footer">
 		<section>
 			<div class="social-icons">
-				<a href="https://www.facebook.com/pages/Knw-photography/521554914554863?ref=br_tf" target="_blank" class="icon-facebook"></a>
+				<a href="https://www.facebook.com/knwphoto" target="_blank" class="icon-facebook"></a>
 				<a href="http://www.pinterest.com/knwphoto/" target="_blank" class="icon-pinterest"></a>
-				<a href="https://instagram.com/kirstennoellew/" target="_blank" class="icon-instagram"></a>
+				<a href="https://instagram.com/knwphoto/" target="_blank" class="icon-instagram"></a>
 			</div>
 			<span class="copyright">&copy; <?php echo date("Y") ?> KNW Photography</span>
 		</section>
