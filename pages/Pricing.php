997ad55677af04258988ec05766664c2e436933b
Ryan Wiemer <ryan@ryanwiemer.com>
2016-12-13 21:50:54 -0800

    Minor tweak to pricing page

--- pages/Pricing.php
+++ pages/Pricing.php
@@ -11,9 +11,9 @@ get_header(); ?>
 
 <section class="pricing">
 
-<?php $family_image_cover = wp_get_attachment_image_src(get_field('family_pricing_image'), 'medium'); ?>
+<?php $lifestyle_image_cover = wp_get_attachment_image_src(get_field('lifestyle_pricing_image'), 'medium'); ?>
 
-	<a href="<?php echo get_site_url(); ?>/pricing/lifestyle" data-bg="<?php echo $family_image_cover[0]; ?>" class="pricing__cover lazyload">
+	<a href="<?php echo get_site_url(); ?>/pricing/lifestyle" data-bg="<?php echo $lifestyle_image_cover[0]; ?>" class="pricing__cover lazyload">
 		<div class="pricing__cover__overlay"></div>
 		<h3 class="pricing__cover__title">Lifestyle</h3>
 	</a>
